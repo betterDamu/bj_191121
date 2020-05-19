7ff1801eecd18519921d6e94c9482907c04df4e3
damu <wjl@126.com>
2020-05-19 09:29:52 +0800

    0519 -1

---
+++ work/day06/03_竖向滑屏/js/base.js
@@ -0,0 +1,159 @@
+//判断用户上来的首次滑屏方向 如果是x轴方向 那以后不管怎么滑动都不会触发滑屏逻辑
+//判断用户上来的首次滑屏方向 如果是y轴方向 那以后不管怎么滑动都会触发滑屏逻辑
+(function (w) {
+    w.scroll = {};
+
+    function init(wrap){
+        //挑选一个适配方案
+        var styleNode = document.createElement("style");
+        var w = document.documentElement.clientWidth/16;
+        styleNode.innerHTML = `html{font-size:${w}px!important}`;
+        document.head.appendChild(styleNode)
+        //禁止移动端事件的默认行为
+        wrap.addEventListener("touchstart",(ev)=>{
+            ev = ev || event;
+            ev.preventDefault();
+        })
+
+        move(wrap)
+    }
+    function move(wrap){
+        //childNodes : 包含文本子节点  firstChild=childNodes[0]
+        //children   : 不包含文本子节点
+        var node = wrap.children[0];
+        /*
+            在计算minY的时候 是有一个潜在的bug的  node这个滑屏元素里面的内容是动态生成的
+            在move方法调用的时候  滑屏元素不一定能渲染成功
+
+            当你项目中遇到一些动态生成的节点时 而且要获取这些节点的位置尺寸这种信息时 一定要长个心眼
+            最好把获取操作发到一个异步逻辑中
+        */
+        var minY = wrap.clientHeight - node.offsetHeight;
+
+        var eleStartX = 0;
+        var eleStartY = 0;
+        var szStartX =0;
+        var szStartY =0;
+
+
+        var lastPoint = 0;
+        var lastTime = 0;
+        var pointDisY = 0;
+        var timeDisY = 0;
+
+        //防抖动需要的变量
+        var isFirst = true; //让一段逻辑只执行一次需要的变量
+        var isY = true; //用户的滑屏方向是否是Y轴
+
+        wrap.addEventListener("touchstart",(ev)=>{
+            ev = ev || event;
+            //防止minY在外部拿的不精确
+            minY = wrap.clientHeight - node.offsetHeight;
+            var touchC = ev.changedTouches[0];
+
+            eleStartX = transform.css(node,"translateX");
+            eleStartY = transform.css(node,"translateY");
+            szStartX  = touchC.clientX;
+            szStartY  = touchC.clientY;
+
+            lastPoint =  touchC.clientY;
+            lastTime = new Date().getTime();
+
+            node.style.transition="";
+            node.handMove = false;
+            pointDisY =0;
+            timeDisY =1;
+
+            //防抖动的值得重新置回来
+            isFirst = true;
+            isY = true;
+        })
+        wrap.addEventListener("touchmove",(ev)=>{
+
+            //看门狗
+            if(!isY){
+                //咬住
+                return;//防的是后续的抖动
+            }
+
+            ev = ev || event;
+            var touchC = ev.changedTouches[0];
+
+            var nowPoint = touchC.clientY; //当次touchmove时 手指的位置
+            var nowTime = new Date().getTime();
+            pointDisY = nowPoint - lastPoint //当次touchmove 距离 上一次touchmove 我们手指移动的距离
+            timeDisY = nowTime - lastTime;
+            lastPoint = nowPoint;
+            lastTime = nowTime;
+
+            var szNowX = touchC.clientX;
+            var szNowY = touchC.clientY;
+            var szDisX = szNowX - szStartX;
+            var szDisY = szNowY - szStartY;
+            var translateY = eleStartY + szDisY;
+
+            var scale = 1;
+            if(translateY > 0){
+                node.handMove = true; //如果为true代表了进行了手动橡皮筋效果的
+                scale = document.documentElement.clientWidth / ((document.documentElement.clientWidth + translateY)*2.5);
+            }else if (translateY < minY) {
+                node.handMove = true;
+                var over = minY - translateY;
+                scale = document.documentElement.clientWidth / ((document.documentElement.clientWidth + over)*2.5);
+            }
+            translateY = transform.css(node,"translateY")+(pointDisY*scale);
+
+
+            //判断用户上来的首次滑屏方向
+            if(isFirst){
+                isFirst = false
+                //如果在手指的滑动方向是y轴 则需要停止整个滑屏逻辑
+                if(Math.abs(szDisX) > Math.abs(szDisY)){
+                    //说明滑动的方向 是偏向y轴的
+                    isY=false;
+                    return; //防的是首次抖动
+                }
+            }
+
+
+            transform.css(node,"translateY",translateY)
+        })
+        wrap.addEventListener("touchend",()=>{
+
+
+            if(node.handMove){
+                var translateY = transform.css(node,"translateY");
+                if(translateY > 0){
+                    translateY = 0;
+                }else if (translateY < minY) {
+                    translateY = minY;
+                }
+                node.style.transition = ".5s transform"
+                transform.css(node,"translateY",translateY);
+            }else{
+                fast()
+            }
+
+
+            function fast() {
+                var speed = pointDisY / timeDisY;
+                speed = Math.abs(speed) < 0.5 ? 0 : speed;
+                var translateY = transform.css(node,"translateY");
+                translateY = translateY + speed*200;
+
+                var bsr = "";
+                if(translateY > 0){
+                    translateY = 0;
+                    bsr = "cubic-bezier(.06,1.85,.83,1.75)";
+                }else if (translateY < minY) {
+                    translateY = minY;
+                    bsr = "cubic-bezier(.06,1.85,.83,1.75)";
+                }
+                node.style.transition = ".5s "+(bsr)+" transform"
+                transform.css(node,"translateY",translateY);
+            }
+        })
+    }
+
+    w.scroll.init = init;
+})(window)
