86f6ce2e6e9e3c872856c419be99af8ac7bc88df
damu <wjl@126.com>
2020-05-20 10:51:36 +0800

    0520 -2

---
+++ work/day07/day07.md
@@ -0,0 +1,6 @@
+### vue2.0 脚手架
+    安装2.0的脚手架 : npm i vue-cli -g
+    查看脚手架的版本 : vue -V
+    查看脚手架可选的模板: vue list
+    初始化项目 : vue init webpack projectname
+    启动项目 : npm start
---
+++ work/day07/vue-cli-demo/demo/src/main.js
@@ -0,0 +1,23 @@
+import Vue from 'vue'
+import App from './App'
+
+Vue.config.productionTip = false
+
+/* eslint-disable no-new */
+
+/*
+  App:{
+    name: 'App',
+    render:function(h){
+      return h("div")
+    }
+  }
+
+   template:`<div id="app">
+      <img src="./assets/logo.png">
+    </div>`
+*/
+new Vue({
+  el: '#app',
+  render: h => h(App)
+})
